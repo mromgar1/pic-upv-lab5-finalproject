45b36a165427f33bc35ce90c4d4e2058704c7826
mromgar1 <mromgar1@upv.edu.es>
2026-04-09 10:03:37 +0200

    arreglo eme shallow

--- src/upvfab_design_tools/eme.py
+++ src/upvfab_design_tools/eme.py
@@ -18,6 +18,9 @@ class MMI_EME:
         wvl=1.55,
         mat_core=mat.sin(1.55),
         mat_cladd=mat.sio2(1.55),
+        mat_shallow=0,
+        sh_width = 0, 
+        sh_thickness = 0, 
         polarization="TE",
         n_IN=2,
         IN_WVG_positions=(-1, 1),
@@ -40,6 +43,9 @@ class MMI_EME:
         self.wvl = wvl
         self.mat_core = mat_core
         self.mat_cladd = mat_cladd
+        self.mat_shallow = mat_shallow
+        self.sh_width = sh_width
+        self.sh_thickness = sh_thickness
         self.polarization = polarization
         self.n_IN = n_IN
         self.IN_WVG_positions = list(IN_WVG_positions)
@@ -48,6 +54,7 @@ class MMI_EME:
         self.power_IN_config = list(power_IN)
         self.wg_width = wg_width
         self.wg_num_modes = wg_num_modes
+        self.wg_thickness = wg_thickness
         self.MMI_width = MMI_width
         self.MMI_num_modes = MMI_num_modes
         self.dz = dz
@@ -106,6 +113,9 @@ class MMI_EME:
             wg_width=self.MMI_width,
             wg_thickness=self.wg_thickness,
             mat_core=self.mat_core,
+            mat_shallow = self.mat_shallow,
+            sh_width = self.sh_width,
+            sh_thickness = self.sh_thickness,
             wvl=self.wvl,
             num_modes=self.MMI_num_modes,
             XY=self.XY,
